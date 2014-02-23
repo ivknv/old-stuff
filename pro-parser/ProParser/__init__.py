c6918729d3afc43cf946ce9d3b9ab1efd35c2741
SPython <rvan.mega@gmail.com>
2014-02-23 22:43:07 +0600

    Added some search functions

--- pro-parser/ProParser/__init__.py
+++ pro-parser/ProParser/__init__.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 from xml.dom import minidom
 from math import ceil
+import datetime
 import os, xml
+
+weekdays=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 class ProjectError(Exception):
 	pass
@@ -195,10 +198,19 @@ def getHalfOfTheDay(path):
 		s=int(date.second)/6000
 		return ceil((h+m+s)/12)
 
+def getWeekNumber(path):
+	date=getDate(path)
+	d=int(date.day)
+	m=int(date.month)
+	y=int(date.year)
+	return datetime.date(y, m, d).isocalendar()[1]
+
 def findProjectByName(name, directory=".", func="", funce="", funcelse=""):
 	if os.path.exists(directory):
 		name=name.lower()
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				if name in getName(d[0]).lower():
@@ -212,6 +224,8 @@ def findProjectByDescription(description, directory=".", func="", funce="", func
 	if os.path.exists(directory):
 		description=description.lower()
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				if description in getDescription(d[0]).lower():
@@ -226,6 +240,8 @@ def findProjectsByName(name, directory=".", func="", funce="", funcelse=""):
 		name=name.lower()
 		projects=[]
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				if name in getName(d[0]).lower():
@@ -241,6 +257,8 @@ def findProjectsByNameAsDict(name, directory=".", func="", funce="", funcelse=""
 		name=name.lower()
 		projects={}
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				gn=getName(d[0])
@@ -257,6 +275,8 @@ def findProjectsByDescription(description, directory=".", func="", funce="", fun
 		description=description.lower()
 		projects=[]
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				if description in getDescription().lower():
@@ -272,10 +292,16 @@ def findProjectsByDescriptionAsDict(description, directory=".", func="", funce="
 		description=description.lower()
 		projects={}
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				if description in getDescription(d[0]).lower():
-					projects[getName(d[0])]=Project(d[0])
+					gn=getName(d[0])
+					if gn:
+						projects[gn]=Project(d[0])
+					else:
+						continue
 			except ProjectError as e:
 				exec(funce)
 			else:
@@ -287,6 +313,8 @@ def findProjectsByLanguage(lang, directory=".", func="", funce="", funcelse="", 
 		lang=lang.lower()
 		projects=[]
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				if not strict:
@@ -306,14 +334,24 @@ def findProjectsByLanguageAsDict(lang, directory=".", func="", funce="", funcels
 		lang=lang.lower()
 		projects={}
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				if not strict:
 					if lang in getLang(d[0]).lower():
-						projects[getName(d[0])]=Project(d[0])
+						gn=getName(d[0])
+						if gn:
+							projects[gn]=Project(d[0])
+						else:
+							continue
 				else:
 					if lang == getLang(d[0]).lower():
-						projects[getName(d[0])]=Project(d[0])
+						gn=getName(d[0])
+						if gn:
+							projects[gn]=Project(d[0])
+						else:
+							continue
 			except ProjectError as e:
 				exec(funce)
 			else:
@@ -325,6 +363,8 @@ def findProjectsByAuthor(author, directory=".", func="", funce="", funcelse=""):
 		author=author.lower()
 		projects=[]
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				if author in [a.lower() for a in getAuthors(d[0])]:
@@ -340,10 +380,17 @@ def findProjectsByAuthorAsDict(author, directory=".", func="", funce="", funcels
 		author=author.lower()
 		projects={}
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				if author in [a.lower() for a in getAuthors(d[0])]:
-					projects[getName(d[0])]=Project(d[0])
+					gn=getName(d[0])
+					if gn:
+						projects[gn]=Project(d[0])
+					else:
+						continue
+			
 			except ProjectError as e:
 				exec(funce)
 			else:
@@ -355,6 +402,8 @@ def findProjectsByAuthors(authors, directory=".", func="", funce="", funcelse=""
 		authors=[a.lower() for a in authors]
 		projects=[]
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				a1=[a.lower() for a in getAuthors(d[0])]
@@ -383,6 +432,8 @@ def findProjectsByAuthorsAsDict(authors, directory=".", func="", funce="", funce
 		authors=[a.lower() for a in authors]
 		projects={}
 		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
 			try:
 				exec(func)
 				a1=[a.lower() for a in getAuthors(d[0])]
@@ -390,14 +441,14 @@ def findProjectsByAuthorsAsDict(authors, directory=".", func="", funce="", funce
 					for author in authors:
 						if author in a1:
 							gn=getName(d[0])
-							if gn not in projects:
+							if gn and gn not in projects:
 								projects[gn]=Project(d[0])
 				else:
 					for author in authors:
 						if author not in a1:
 							break
 						gn=getName(d[0])
-						if gn not in projects:
+						if gn and gn not in projects:
 							projects[gn]=Project(d[0])
 
 			except ProjectError as e:
@@ -406,25 +457,490 @@ def findProjectsByAuthorsAsDict(authors, directory=".", func="", funce="", funce
 				exec(funcelse)
 		return projects
 
+def findProjectsByReferenced(referenced, directory=".", func="", funce="", funcelse="", strict=False):
+	if os.path.exists(directory):
+		referenced=referenced.lower()
+		projects=[]
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				r=getReferenced(d[0]).lower()
+				if not strict:
+					if referenced in r:
+						projects.append(Project(d[0]))
+				else:
+					if referenced == r:
+						projects.append(Project(d[0]))
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByReferencedAsDict(referenced, directory=".", func="", funce="", funcelse="", strict=False):
+	if os.path.exists(directory):
+		referenced=referenced.lower()
+		projects={}
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				r=getReferenced(d[0]).lower()
+				if not strict:
+					if referenced in r:
+						gn=getName(d[0])
+						if gn:
+							projects[gn]=Project(d[0])
+						else:
+							continue
+				else:
+					if referenced == r:
+						gn=getName(d[0])
+						if gn:
+							projects[gn]=Project(d[0])
+						else:
+							continue
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByVersion(version, directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		version=version.lower()
+		projects=[]
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				v=getVersion(d[0]).lower()
+				if not strict:
+					if version in v:
+						projects.append(Project(d[0]))
+				else:
+					if version == v:
+						projects.append(Project(d[0]))
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByVersionAsDict(version, directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		version=version.lower()
+		projects={}
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				v=getVersion(d[0]).lower()
+				if not strict:
+					if version in v:
+						gn=getName(d[0])
+						if gn:
+							projects[gn]=Project(d[0])
+						else:
+							continue
+				else:
+					if version == v:
+						gn=getName(d[0])
+						if gn:
+							projects[gn]=Project(d[0])
+						else:
+							continue
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByYearAsDict(year, directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		projects={}
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				y=getYear(d[0]).lower()
+				if year == y:
+					gn=getName(d[0])
+					if gn:
+						projects[gn]=Project(d[0])
+					else:
+						continue
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByYear(year, directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		projects=[]
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				y=getYear(d[0]).lower()
+				if year == y:
+						projects.append(Project(d[0]))
+				else:
+					continue
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByMonth(month, directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		if len(month) == 1:
+			month="0"+month
+		projects=[]
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				m=getMonth(d[0]).lower()
+				if month == m:
+						projects.append(Project(d[0]))
+				else:
+					continue
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByMonthAsDict(month, directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		if len(month) == 1:
+			month="0"+month
+		projects={}
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				m=getMonth(d[0]).lower()
+				if month == m:
+					gn=getName(d[0])
+					if gn:
+						projects[gn]=Project(d[0])
+				else:
+					continue
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByWeek(week=datetime.date(year=datetime.datetime.now().year, month=datetime.datetime.now().month, day=datetime.datetime.now().day).isocalendar()[1], directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		if isinstance(week, str):
+			week=int(week)
+		projects=[]
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				w=getWeekNumber(d[0])
+				if week == w:
+						projects.append(Project(d[0]))
+				else:
+					continue
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByWeekAsDict(week=datetime.date(year=datetime.datetime.now().year, month=datetime.datetime.now().month, day=datetime.datetime.now().day).isocalendar()[1], directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		if isinstance(week, str):
+			week=int(week)
+		projects={}
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				w=getWeekNumber(d[0])
+				if week == w:
+					gn=getName(d[0])
+					if gn:
+						projects[gn]=Project(d[0])
+				else:
+					continue
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByDate(year="", month="", day="", weekday="", directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		year=str(year)
+		month=str(month)
+		if len(month) == 1:
+			month="0"+month
+		day=str(day)
+		if len(day) == 1:
+			day="0"+day
+		projects=[]
+		for d in os.walk(directory):
+			if not isProject(d[0]):
+				continue
+			try:
+				exec(func)
+				if year:
+					y=getYear(d[0])
+				if month:
+					m=getMonth(d[0])
+				if day:
+					d1=getDay(d[0])
+				if weekday:
+					w=getWeekday(d[0])
+				
+				if year and not month and not day and not weekday:
+					if y == year:
+						projects.append(Project(d[0]))
+				elif year and month and not day and not weekday:
+					if y == year and m == month:
+						projects.append(Project(d[0]))
+				elif year and month and day and not weekday:
+					if y == year and m == month and d1 == day:
+						projects.append(Project(d[0]))
+				elif not year and month and not day and not weekday:
+					if m == month:
+						projects.append(Projects(d[0]))
+				elif not year and month and day and not weekday:
+					if m == month and d1 == day:
+						projects.append(Project(d[0]))
+				elif not year and not month and day and not weekday:
+					if d1 == day:
+						projects.append(Project(d[0]))
+				elif year and month and not day and weekday:
+					if y == year and month == m and w == weekday:
+						projects.append(Project(d[0]))
+				elif year and month and day and weekday:
+					if y == year and m == month and d1 == day and w == weekday:
+						projects.append(Project(d[0]))
+				elif not year and month and not day and weekday:
+					if m == month and w == weekday:
+						projects.append(Project(d[0]))
+				elif not year and month and day and weekday:
+					if m == month and d1 == day and w == weekday:
+						projects.append(Project(d[0]))
+				elif year and not month and not day and weekday:
+					if y == year and w == weekday:
+						projects.append(Project(d[0]))
+				elif year and not month and day and weekday:
+					if y == year and d1 == day and w == weekday:
+						projects.append(Project(d[0]))
+				elif not year and not month and not day and weekday:
+					if w == weekday:
+						projects.append(Project(d[0]))
+
+			except ProjectError as e:
+				exec(funce)
+			else:
+				exec(funcelse)
+		return projects
+
+def findProjectsByDateAsDict(year="", month="", day="", weekday="", directory=".", func="", funce="", funcelse=""):
+	if os.path.exists(directory):
+		year=str(year)
+		month=str(month)
+		if len(month) == 1:
+			month="0"+month
+		day=str(day)
+		if len(day) == 1:
+			day="0"+day
+		projects={}
+		for d in os.walk(directory):
+			if isProject(d[0]):
+				try:
+					exec(func)
+					if year:
+						y=getYear(d[0])
+					if month:
+						m=getMonth(d[0])
+					if day:
+						d1=getDay(d[0])
+					if weekday:
+						w=getWeekday(d[0])
+					
+					if year and not month and not day and not weekday:
+						if y == year:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif year and month and not day and not weekday:
+						if y == year and m == month:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif year and month and day and not weekday:
+						if y == year and m == month and d1 == day:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif not year and month and not day and not weekday:
+						if m == month:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Projects(d[0])
+							else:
+								continue
+					elif not year and month and day and not weekday:
+						if m == month and d1 == day:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif not year and not month and day and not weekday:
+						if d1 == day:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif year and month and not day and weekday:
+						if y == year and month == m and w == weekday:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif year and month and day and weekday:
+						if y == year and m == month and d1 == day and w == weekday:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif not year and month and not day and weekday:
+						if m == month and w == weekday:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+					elif not year and month and day and weekday:
+						if m == month and d1 == day and w == weekday:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif year and not month and not day and weekday:
+						if y == year and w == weekday:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif year and not month and day and weekday:
+						if y == year and d1 == day and w == weekday:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+					elif not year and not month and not day and weekday:
+						if w == weekday:
+							gn=getName(d[0])
+							if gn:
+								projects[gn]=Project(d[0])
+							else:
+								continue
+			
+				except ProjectError as e:
+					exec(funce)
+				else:
+					exec(funcelse)
+		return projects
+
 class Project(object):
 	def __init__(self, path):
 		self.name=getName(path)
 		self.language=getLang(path)
-		self.description=getDescription(path)
-		self.authors=getAuthors(path)
-		self.version=getVersion(path)
-		self.date=getDate(path)
-		self.day=self.date.day
-		self.weekday=self.date.weekday
-		self.month=self.date.month
-		self.year=self.date.year
-		self.hour=self.date.hour
-		self.minute=self.date.minute
-		self.second=self.date.second
-		self.referenced=getReferenced(path)
+		try:
+			self.description=getDescription(path)
+		except ProjectError:
+			self.description=None
+		try:
+			self.authors=getAuthors(path)
+		except ProjectError:
+			self.authors=None
+		try:
+			self.version=getVersion(path)
+		except ProjectError:
+			self.version=None
+		try:
+			self.date=getDate(path)
+			self.day=self.date.day
+			self.weekday=self.date.weekday
+			self.month=self.date.month
+			self.year=self.date.year
+			self.hour=self.date.hour
+			self.minute=self.date.minute
+			self.second=self.date.second
+			self.referenced=getReferenced(path)
+		except ProjectError:
+			self.date=None
+			self.day=None
+			self.weekday=None
+			self.month=None
+			self.year=None
+			self.hour=None
+			self.minute=None
+			self.second=None
+			self.referenced=None
 		self.fullpath=os.path.realpath(path)
 		self.as_dict={"name": self.name, "language": self.language, "description": self.description, "authors": self.authors, "version": self.version, "date": {"day": self.day, "weekday": self.weekday, "month": self.month, "year": self.year, "hour": self.hour, "minute": self.minute, "second": self.second}, "referenced": self.referenced, "fullpath": self.fullpath}
 		self.keys=["name", "version", "language", "description", "authors", "date", "referenced"]
+		k1=[kk for kk in self.as_dict]
+		k2=[kk for kk in self.as_dict["date"]]
+		for i in range(len(self.as_dict)):
+			if not self.as_dict[k1[i]]:
+				if i in self.keys:
+					self.keys.remove(k1[i])
+				self.as_dict.pop(k1[i])
+			elif self.as_dict[k1[i]] and isinstance(self.as_dict[k1[i]], dict):
+				for ii in range(len(self.as_dict[k1[i]])):
+					if not self.as_dict[k1[i]][k2[ii]]:
+						self.as_dict[k1[i]].pop(k2[ii])
+					if not self.as_dict[k1[i]]:
+						self.as_dict.pop(k1[i])
+		
 		self.n = -1
 	def __call__(self, key=None, key1=None):
 		if key and key1:
