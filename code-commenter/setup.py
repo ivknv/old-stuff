4be227be664631e8988df676fdd25ef5ee4a7054
SPython <rvan.mega@gmail.com>
2014-03-16 00:39:55 +0600

    Added setup.py, install.sh, main script (ccommenter.py), examples

---
+++ code-commenter/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name="CodeCommenter",
+version="1.0",
+author="Ivan Konovalov",
+author_email="rvan.mega@gmail.com",
+description="CodeCommenter allows to automaticaly generate comments using XML file which contains patterns, texts, and some other settings",
+packages=["CodeCommenter"])
