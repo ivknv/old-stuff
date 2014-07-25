787ca964eca4d2f5b7933b16d3f23cac3b12e78d
SPython <rvan.mega@gmail.com>
2014-07-25 23:48:41 +0600

    Improve code

--- daemo/Daemonizer/__init__.py
+++ daemo/Daemonizer/__init__.py
@@ -1,30 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Simple module for writting daemons.
+
+Includes functions for starting, stopping and restarting daemons.
+"""
+
 import os, time, signal, sys
 
-def start_daemon(c):
-	pid=os.fork()
-	c=c()
-	f1=open(c.pidfile_path, "w")
-	f1.write(str(os.getpid()))
-	f1.close()
-	if pid!=0:
-		time.sleep(10)
+def start_daemon(obj):
+	"""Start daemon"""
+	
+	pid = os.fork()
+	obj = obj()
+	f = open(obj.pidfile_path, "w")
+	f.write(str(os.getpid()))
+	f.close()
+	
+	if pid != 0:
 		sys.exit(0)
-	c.start()
-	if c.pidfile_autoremove:
-		os.remove(c.pidfile_path)
+	
+	if hasattr(obj, "onStart"):
+		obj.onStart()
+	
+	if obj.pidfile_autoremove:
+		os.remove(obj.pidfile_path)
 
-def stop_daemon(c):
-	c=c()
-	f1=open(c.pidfile_path)
-	pid=f1.read()
-	f1.close()
-	os.remove(c.pidfile_path)
-	c.stop()
+def stop_daemon(obj):
+	"""Stop daemon"""
+	
+	obj = obj()
+	f = open(obj.pidfile_path)
+	pid = f.read()
+	f.close()
+	os.remove(obj.pidfile_path)
+	
+	if hasattr(obj, "onStop"):
+		obj.onStop()
+	
 	os.kill(int(pid), signal.SIGTERM)
 
-def restart_daemon(c):
-	stop_daemon(c)
-	start_daemon(c)
+def restart_daemon(obj):
+	"""Restart daemon"""
+	
+	stop_daemon(obj)
+	
+	if hasattr(obj, "onRestart"):
+		obj.onRestart()
+	
+	start_daemon(obj)
