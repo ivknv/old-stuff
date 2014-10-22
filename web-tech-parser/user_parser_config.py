6c1313aa13e7b5b825dce41a18cca7ac68372b86
SPython <rvan.mega@gmail.com>
2014-10-22 21:52:54 +0600

    Update requirements and configuration

--- web-tech-parser/user_parser_config.py
+++ web-tech-parser/user_parser_config.py
@@ -6,7 +6,7 @@ from TechParser import venturebeat
 from TechParser import engadget
 from TechParser import techcrunch
 from TechParser import techrepublic
-#from TechParser import readwrite
+from TechParser import readwrite
 from TechParser import smashingmagazine
 from TechParser import gizmodo
 from TechParser import slashdot
@@ -34,186 +34,213 @@ from TechParser import mobilereview
 from TechParser import helpix
 from TechParser import recode
 from TechParser import zdnet
+from TechParser import geektimes
 
 sites_to_parse = {
-		"Habrahabr": { # habrahabr.ru
-			"module": habrahabr,
-			"kwargs": {"hubs": []}
-		},
-		
-		"VentureBeat": { # venturebeat.com
-			"module": venturebeat,
-			"kwargs": {"start_page": 1, "end_page": 3}
-		},
-		
-		"Engadget": { # engadget.com
-			"module": engadget,
-			"kwargs": {"start_page": 1, "end_page": 5}
-		},
-		
-		"Slashdot": { # slashdot.org
-			"module": slashdot,
-			"kwargs": {"start_page": 1, "end_page": 3}
-		},
-		
-		"Gizmodo": { # gizmodo.com
-			"module": gizmodo,
-			"kwargs": {}
-		},
-		
-		"TechCrunch": { # techcrunch.com
-			"module": techcrunch,
-			"kwargs": {}
-		},
-		
-		# Works only with Selenium (opens REAL browser)
-		#"Read/Write Web": { # readwrite.com
-		#	"module": readwrite,
-		#	"kwargs": {"browser": "firefox"}
-		#},
-		
-		"Tech Republic": { # techrepublic.com
-			"module": techrepublic,
-			"kwargs": {}
-		},
-		
-		"Smashing Magazine": { # www.smashingmagazine.com
-			"module": smashingmagazine,
-			"kwargs": {}
-		},
-		
-		"Android Central": { # www.androidcentral.com
-			"module": androidcentral,
-			"kwargs": {"start_page": 1, "end_page": 5}
-		},
-		
-		"The Verge": { # www.theverge.com
-			"module": verge,
-			"kwargs": {}
-		},
-		
-		"Top Design Magazine": { # www.topdesignmag.com
-			"module": topdesignmag,
-			"kwargs": {"start_page": 1, "end_page": 1}
-		},
-		
-		"Flowa": { # flowa.fi
-			"module": flowa,
-			"kwargs": {}
-		},
-		
-		"IT Toolbox": { # it.toolbox.com
-			"module": ittoolbox,
-			"kwargs": {}
-		},
-		
-		"DZone": { # www.dzone.com
-			"module": dzone,
-			"kwargs": {}
-		},
-		
-		"Code Project": { # www.codeproject.com
-			"module": codeproject,
-			"kwargs": {"start_page": 1, "end_page": 1}
-		},
-		
-		"Hacker News": { # news.ycombinator.com/newest
-			"module": hackernews,
-			"kwargs": {"start_page": 1, "end_page": 5}
-		},
-		
-		"Mashable": { # mashable.com
-			"module": mashable,
-			"kwargs": {}
-		},
-		
-		"Make Tech Easier": { # www.maketecheasier.com
-			"module": maketecheasier,
-			"kwargs": {}
-		},
-		
-		"Digg": { # digg.com
-			"module": digg,
-			"kwargs": {}
-		},
-		
-		"Wired": { # www.wired.com
-			"module": wired,
-			"kwargs": {}
-		},
-		
-		"Medium": { # medium.com
-			"module": medium,
-			"kwargs": {"collections": ["programming-stories",
-				"python-programming-language",
-				"programming-ideas-tutorial-and-experience",
-				"software-development-2", "developer-developers-developers",
-				"web-design-and-development", "desenvolvimento-web",
-				"web-development-9", "coding-design", "cool-code-pal",
-				"html-css-and-happiness", "html-css", "front-end-developers",
-				"web-design-tutorials", "written-in-code", "ui-designs",
-				"design-creativity-1", "this-could-be-better",
-				"user-experience-design-1", "design-ui-and-shenanigans"]}
-		},
-		
-		"Planet Clojure": { # planet.clojure.in
-			"module": planetclojure,
-			"kwargs": {}
-		},
-		
-		"Reddit": { # www.reddit.com
-			"module": reddit,
-			"kwargs": {"reddits": ["tech", "programming",
-				"clojure", "python", "html", "css",
-				"html5", "java", "javascript", "google",
-				"django", "functionalprogramming"]}
-		},
-		
-		"Trashbox": { # trashbox.ru
-			"module": trashbox,
-			"kwargs": {"end_page": 3}
-		},
-		
-		"Droider": { # droider.ru
-			"module": droider,
-			"kwargs": {"start_page": 1, "end_page": 2}
-		},
-		
-		"Redroid": { # redroid.ru
-			"module": redroid,
-			"kwargs": {"start_page": 1, "end_page": 1}
-		},
-		
-		"3DNews": { # www.3dnews.ru
-			"module": threednews,
-			"kwargs": {}
-		},
-		
-		"IXBT": { # www.ixbt.ru
-			"module": ixbt,
-			"kwargs": {}
-		},
-		
-		"Mobile Review": { # mobile-review.com
-			"module": mobilereview,
-			"kwargs": {}
-		},
-		
-		"Helpix": { # helpix.ru
-			"module": helpix,
-			"kwargs": {}
-		},
-		
-		"Re/code": { # recode.net
-			"module": recode,
-			"kwargs": {}
-		},
-		
-		"ZDNet": { # www.zdnet.com
-			"module": zdnet,
-			"kwargs": {}
-		}
+	"Habrahabr": { # habrahabr.ru
+		"module": habrahabr,
+		"kwargs": {"hubs": ['programming', 'python', 'javascript', 'c',
+			'webdev', 'linux', 'open_source', 'development', 'web_design',
+			'mobile', 'css', 'html5', 'crazydev', 'jquery', 'funcprog']}
+	},
+	
+	"VentureBeat": { # venturebeat.com
+		"module": venturebeat,
+		"kwargs": {}
+	},
+	
+	"Engadget": { # engadget.com
+		"module": engadget,
+		"kwargs": {}
+	},
+	
+	"Slashdot": { # slashdot.org
+		"module": slashdot,
+		"kwargs": {}
+	},
+	
+	"Gizmodo": { # gizmodo.com
+		"module": gizmodo,
+		"kwargs": {}
+	},
+	
+	"TechCrunch": { # techcrunch.com
+		"module": techcrunch,
+		"kwargs": {}
+	},
+	
+	"Read/Write Web": { # readwrite.com
+		"module": readwrite,
+		"kwargs": {}
+	},
+	
+	"Tech Republic": { # techrepublic.com
+		"module": techrepublic,
+		"kwargs": {}
+	},
+	
+	"Smashing Magazine": { # www.smashingmagazine.com
+		"module": smashingmagazine,
+		"kwargs": {}
+	},
+	
+	"Android Central": { # www.androidcentral.com
+		"module": androidcentral,
+		"kwargs": {}
+	},
+	
+	"The Verge": { # www.theverge.com
+		"module": verge,
+		"kwargs": {}
+	},
+	
+	"Top Design Magazine": { # www.topdesignmag.com
+		"module": topdesignmag,
+		"kwargs": {}
+	},
+	
+	"Flowa": { # flowa.fi
+		"module": flowa,
+		"kwargs": {}
+	},
+	
+	"IT Toolbox": { # it.toolbox.com
+		"module": ittoolbox,
+		"kwargs": {}
+	},
+	
+	"DZone": { # www.dzone.com
+		"module": dzone,
+		"kwargs": {}
+	},
+	
+	"Code Project": { # www.codeproject.com
+		"module": codeproject,
+		"kwargs": {'categories': ['all']}
+	},
+	
+	"Hacker News": { # news.ycombinator.com
+		"module": hackernews,
+		"kwargs": {}
+	},
+	
+	"Mashable": { # mashable.com
+		"module": mashable,
+		"kwargs": {}
+	},
+	
+	"Make Tech Easier": { # www.maketecheasier.com
+		"module": maketecheasier,
+		"kwargs": {}
+	},
+	
+	"Digg": { # digg.com
+		"module": digg,
+		"kwargs": {}
+	},
+	
+	"Wired": { # www.wired.com
+		"module": wired,
+		"kwargs": {}
+	},
+	
+	"Medium": { # medium.com
+		"module": medium,
+		"kwargs": {"collections": ["programming-stories",
+			"python-programming-language",
+			"programming-ideas-tutorial-and-experience",
+			"software-development-2", "developer-developers-developers",
+			"web-design-and-development", "desenvolvimento-web",
+			"web-development-9", "coding-design", "cool-code-pal",
+			"html-css-and-happiness", "html-css", "front-end-developers",
+			"web-design-tutorials", "written-in-code", "ui-designs",
+			"design-creativity-1", "this-could-be-better",
+			"user-experience-design-1", "design-ui-and-shenanigans"]}
+	},
+	
+	"Planet Clojure": { # planet.clojure.in
+		"module": planetclojure,
+		"kwargs": {}
+	},
+	
+	"Reddit": { # www.reddit.com
+		"module": reddit,
+		"kwargs": {"reddits": ["tech", "programming",
+			"clojure", "python", "html", "css",
+			"html5", "java", "javascript", "google",
+			"django", "functionalprogramming"]}
+	},
+	
+	"Trashbox": { # trashbox.ru
+		"module": trashbox,
+		"kwargs": {}
+	},
+	
+	"Droider": { # droider.ru
+		"module": droider,
+		"kwargs": {}
+	},
+	
+	"Redroid": { # redroid.ru
+		"module": redroid,
+		"kwargs": {}
+	},
+	
+	"3DNews": { # www.3dnews.ru
+		"module": threednews,
+		"kwargs": {}
+	},
+	
+	"IXBT": { # www.ixbt.ru
+		"module": ixbt,
+		"kwargs": {}
+	},
+	
+	"Mobile Review": { # mobile-review.com
+		"module": mobilereview,
+		"kwargs": {}
+	},
+	
+	"Helpix": { # helpix.ru
+		"module": helpix,
+		"kwargs": {}
+	},
+	
+	"Re/code": { # recode.net
+		"module": recode,
+		"kwargs": {}
+	},
+	
+	"ZDNet": { # www.zdnet.com
+		"module": zdnet,
+		"kwargs": {}
+	},
+	
+	"Geektimes": { # geektimes.ru
+		"module": geektimes,
+		"kwargs": {'hubs': ['android', 'hr', 'yandex', 'history', 'google',
+			'soft', 'infographics', 'browsers', 'smartphones', 'geektimes',
+			'iTablet', 'business-laws', 'os', 'internet_of_things',
+			'design', 'gmail', 'statistics', 'internet_regulation',
+			'mailru', 'youtube', 'cpu', 'linux', 'announcements', 'closet',
+			'mozilla', 'wikipedia', 'cyberpunk', 'ascii', 'e_gov', 'rkn']}
 	}
+}
+
+rss_feeds = {'CSS-tricks': {
+		'short-name': 'css-tricks',
+		'url': 'http://feeds.feedburner.com/CssTricks?format=xml',
+		'icon': 'http://css-tricks.com/favicon.ico',
+		'color': '#DA8817'
+	},
+	
+	'The Next Web':	{
+		'url': 'http://feeds2.feedburner.com/thenextweb',
+		'short-name': 'nextweb',
+		'icon': 'http://thenextweb.com/favicon.ico',
+		'color': '#F15A2F'
+	}
+}
 
 filters = {
 	"All": {
@@ -229,8 +256,10 @@ db = 'postgresql'
 host = "0.0.0.0" # Server host
 port = "8080" # Server port
 
-# Server to use
-server = "auto" # See http://bottlepy.org/docs/dev/deployment.html#switching-the-server-backend
+num_threads = 2 # Number of threads for parsing articles
 
-save_articles = False # Save articles into db.
+# Server to use
+server = "tornado" # See http://bottlepy.org/docs/dev/deployment.html#switching-the-server-backend
+
+save_articles = True # Save articles into db.
 # Can be found at ~/.tech-parser/archive.db
