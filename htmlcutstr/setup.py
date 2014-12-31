5810983d71a29b914f2a6bfe8a6e4341d2c7d026
SPython <rvan.mega@gmail.com>
2014-12-31 23:28:34 +0500

    Update data

--- htmlcutstr/setup.py
+++ htmlcutstr/setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(name='htmlcutstr',
-	version='1.2',
+	version='1.2.1',
 	description='Python module which helps to cut HTML strings by keeping HTML tags as they are',
-	author='Prajwala',
-	author_email='m.prajwala@gmail.com',
+	author='Ivan Konovalov',
+	author_email='rvan.mega@gmail.com',
 	py_modules=['htmlcutstr'],
-	data_files=[('README.md')])
+	data_files=[('README.txt')])
