641b02aefa47ab4e17604abb6c99b490b979a7c0
SPython <rvan.mega@gmail.com>
2014-12-31 22:27:16 +0500

    Add new functionality; Rename module to htmlcutstr

--- htmlcutstr/setup.py
+++ htmlcutstr/setup.py
@@ -1,10 +1,9 @@
 from distutils.core import setup
 
-setup(name='htmlcutstring',
-      version='1.1',
-      description='Python module which help to cut html string by keeping html tags as it is',
-      author='Prajwala',
-      author_email='m.prajwala@gmail.com',
-      py_modules=['htmlcutstring'],
-      data_files=[('README.txt')]
-      )
+setup(name='htmlcutstr',
+	version='1.2',
+	description='Python module which help to cut html string by keeping html tags as it is',
+	author='Prajwala',
+	author_email='m.prajwala@gmail.com',
+	py_modules=['htmlcutstr'],
+	data_files=[('README.txt')])
